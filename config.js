3f2549016abce310bd282fb2d8f6f4e92251b99a
ayush <47570543+ayushmoney@users.noreply.github.com>
2021-08-04 12:21:20 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+const dialogflowConfig ={
+    "type": "service_account",
+    "project_id": "bottest-qllg",
+    "private_key_id": "4bae257d72fad03e574dfc9bd353b9df16cbd124",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCLlzRsCTm5ZM+2\na9hLnG0+eG4jW4RbzX6VCDZUNql71Zl1vWX4kItebz3uwj9pmQfij6RIVlBU7ksU\nZjQoYsA/Qy8o6mprVe5irT34feYYdb1P/dzM1ZUhj2euToRp0R6pXxJI3rLslKcw\nAanE3DM/DPQK1UNLyhEXlNFE2cPN+w2OWJc3AnByiQEYzpqHWyLhXLqcrse+wczs\n+bb4dXWfRXw/wOv1U+n5MrH29xUKUt7SKPyNtlBd3M35aNap3Cp+N8y3m8b0WjV+\nklusEXGNdll1t/Zj9soyOBqpa0YnbC+yl7Ogov4l70ied/vjHu6s9UnpCKJKUosG\n/eRSaD0tAgMBAAECggEADjD7Xp9qe69k0Zwxs3AuCAuY0OXpNScVXyPkWBftj54l\n2Zs9fnF9hcaD8p3TchIhg9ZMIqGJzbSjM+n3FAMEHfpf1UZNtudX0rbfpb9w8Nwp\neLQrqjRtkqmgZg9r9VESD7CPbJKdeKFdROtqXc3wNNEFj4NicdZTegUCaIBGII7m\nv5GBWKPz019QGCUgfWMQcBoDqM+pqBJf4kZmZLF8sV/itnuzvYiYYb1RMyl0CZUo\nyBFnyf5jZARBo9wxUIPkwaLsK/h/l1HWPf0/oI7opBNccZeyoxK1ZBNL2q4tuANW\nckRBvEp78ua1pwXmLvJpfF8cRck3dKkzORKhoJ44wQKBgQC/QoazYRe3U389lcmn\nrqagZNrNCRzb6YYd7MBZW7fRqb4J3sfI0LkJPDY1iZvuZvaP4UpZFv9VP6riPrXi\nqKUuBSZxHpw5Eml621l7knmRJejXbE45RpSjbWRYDSbBfnsYZU9Nfa9Ke/+EMMhH\nTbuW3WKCXiu5joo9WHnRxZs2wQKBgQC611KPlDPrYAZMH/eQEjZHcZZ258odj9wo\nPNCvvx1HU9zz72Y6xf7nml6XPzbaaAdQMMTS9TlQksrRQuJ5o9vxb1SV4RKNQ+RQ\n8ZrFica0v1koDYm8ZL/p27DnbgPlMupA3IsNbTRqWS0FOpKfBljFEGrX4EHsIQeP\noheAdTItbQKBgDK3D0TNgB5qk5je+SPvmdt6ItKvkel2WMvilXacbXe5DN1NJ6jG\nP+qE/tLfdGuY/dRVJwPMp1QRl9x6SHybv4EeeH0SYtZ3XWToeSfdRUMJzF4jJ/Lk\nFlHnle4LPrWcNkPXyVmA9FRJ4Q1KajEOGan9djufgPSpKVru9Q582ZWBAoGAWAT8\nNfuhge8YyF/+5nZN79ZJBfcoNkkBdEtCHKkyB9owKxLK3EFOVplYmnQDSVzcpSB0\neNVhjN26gl66Bof1I4ZJGNqRPM+RDbjkKs8/NwM14zKNgp0L9AW1DzG60AzuYmQD\nde3OVZNxLBdt31DEHQIg81YIeE8PGtFHtRKA8vkCgYBuZL4DGKolhEFJD1clgEIm\n6L5+3f2BRiq7ONtnsRNjyOhRrJ57gR6b+C8qMPwCCHJ0f5UfBFJgrAG50FFmG4nw\nvdxDRvqplIzm7cTPuL2xWGJbnp5XHXdvEDXDHTbX9W3uac5GD8aqmDu/NAqemAhH\nVRxaeqfd3ayIOPjHqkXpew==\n-----END PRIVATE KEY-----\n",
+    "client_email": "bottest-qllg@bottest-qllg.iam.gserviceaccount.com",
+    "client_id": "101148307271566599143",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/bottest-qllg%40bottest-qllg.iam.gserviceaccount.com"
+  };
+  module.exports = dialogflowConfig;
+  
